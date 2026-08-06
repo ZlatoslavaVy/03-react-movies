1124188e74599e76884a32b43ff6e91a57b8b4e1
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-08-06 14:59:45 +0300

    add all components, json, declarations, movie.ts, movieService, change main.tsx

---
+++ src/components/App/App/App.tsx
@@ -0,0 +1,9 @@
+// import { useState } from "react";
+import SearchBar from "../../SearchBar/SearchBar";
+// import css from "./App.module.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "modern-normalize";
 import "./index.css";
-import App from "./components/App/App.tsx";
+import App from "./components/App/App/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
