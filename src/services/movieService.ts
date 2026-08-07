2f8d5b87b9df2a8498ae3c792df9c30a36849f79
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-08-07 12:31:33 +0300

    add TOKEN

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,16 +1,22 @@
 import axios from "axios";
 
-const response = await axios.get("", {
-  params: { query },
-});
+const response = await axios.get(
+  "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1",
+  {
+    params: { query },
+  },
+);
 
 async function getMovies(query) {
-  const response = await axios.get("", {
-    params: {
-      // твої параметри
+  const response = await axios.get(
+    "https://api.themoviedb.org/3/search/movie",
+    {
+      params: {
+        // твої параметри
+      },
+      headers: {
+        Authorization: `Bearer  ${import.meta.env.VITE_API_KEY}`,
+      },
     },
-    headers: {
-      Authorization: `Bearer твійТокен`,
-    },
-  });
+  );
 }
