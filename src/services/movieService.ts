5245c7e7d00b051b0c07ec46630492b3ca4e0622
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-08-07 16:06:22 +0300

    do movieSercvice.ts

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,16 +1,29 @@
 import axios from "axios";
+import type { AxiosInstance } from "axios";
+import type { Movie } from "../types/movie";
 
 const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
+export const MOVIE_IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
-async function getMovies(query) {
-  const response = await axios.get(
-    "https://api.themoviedb.org/3/search/movie",
-    {
-      params: { query },
-      headers: {
-        Authorization: `Bearer  ${API_KEY}`,
-      },
-    },
-  );
-  console.log(response.data);
+const tmdbApi: AxiosInstance = axios.create({
+  baseURL: " https://api.themoviedb.org/3",
+  headers: {
+    Authorization: `Bearer  ${API_KEY}`,
+    accept: "application/json",
+  },
+});
+
+interface MoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
 }
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  const response = await tmdbApi.get<MoviesResponse>(`/search/movie`, {
+    params: { query },
+  });
+
+  return response.data.results;
+};
