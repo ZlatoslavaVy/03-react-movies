90d5539bc6b32684c88205f729309b79bace0c12
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-08-08 21:49:24 +0300

    add useState and msg if no movies

--- src/components/App/App/App.tsx
+++ src/components/App/App/App.tsx
@@ -1,12 +1,27 @@
-// import { useState } from "react";
+import { useState } from "react";
 import SearchBar from "../../SearchBar/SearchBar";
 import css from "./App.module.css";
+import { fetchMovies } from "../../../services/movieService";
+import type { Movie } from "../../../types/movie";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
-  const handleSubmit = (topic: string) => {
-    console.log("Query:", topic);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleSubmit = async (topic: string) => {
+    const results = await fetchMovies(topic);
+
+    if (results.length === 0) {
+      toast("No movies found for your request.");
+    }
+    setMovies(results);
   };
-  return <SearchBar onSubmit={handleSubmit} />;
+  return (
+    <>
+      <SearchBar onSubmit={handleSubmit} />
+      <Toaster />
+    </>
+  );
 }
 
 export default App;
