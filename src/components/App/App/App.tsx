25d4ee3d24283aa147d85f4173df68346acfe539
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-08-09 11:48:53 +0300

    add Loader

--- src/components/App/App/App.tsx
+++ src/components/App/App/App.tsx
@@ -5,28 +5,37 @@ import { fetchMovies } from "../../../services/movieService";
 import type { Movie } from "../../../types/movie";
 import toast, { Toaster } from "react-hot-toast";
 import MovieGrid from "../../MovieGrid/MovieGrid";
+import Loader from "../../Loader/Loader";
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleSubmit = async (topic: string) => {
+    setIsLoading(true);
     const results = await fetchMovies(topic);
 
     if (results.length === 0) {
       toast("No movies found for your request.");
     }
     setMovies(results);
+    setIsLoading(false);
   };
 
   const handleSelect = (movie: Movie) => {
     console.log("Selected movie:", movie);
   };
   return (
-    <>
+    <div className={css.app}>
       <SearchBar onSubmit={handleSubmit} />
       <Toaster />
-      <MovieGrid movies={movies} onSelect={handleSelect} />
-    </>
+
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <MovieGrid movies={movies} onSelect={handleSelect} />
+      )}
+    </div>
   );
 }
 
